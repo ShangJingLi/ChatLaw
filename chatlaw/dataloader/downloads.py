7be82894161caa9694f401ddafdcd4d15a2c6e1a
ShangjingLee <1755397894@qq.com>
2025-11-30 00:57:28 +0800

    限制gradio版本，更换下载逻辑

--- chatlaw/dataloader/downloads.py
+++ chatlaw/dataloader/downloads.py
@@ -9,11 +9,9 @@ def download_resources(resource_type):
     if resource_type == "tokenizer":
         openi.openi_download_file("enter/QwenTokenizer", repo_type="dataset" , local_dir=get_resources_path(), max_workers=10)
     elif resource_type == "llm":
-        # openi_download_file("enter/QwenModel", repo_type="model", local_dir=os.path.join(get_resources_path(), "llm"), max_workers=10)
-        openi.download_model(repo_id="enter/ChatLaw",
-                             model_name="QwenModel", save_path=os.path.join(get_resources_path(), "llm"))
+        openi.openi_download_file("enter/QwenModel", repo_type="model", local_dir=os.path.join(get_resources_path(), "llm"), max_workers=10)
     elif resource_type == "video_model":
-        openi.openi_download_file("enter/QwenModel", repo_type="dataset", local_dir=get_resources_path(), max_workers=10)
+        openi.openi_download_file("enter/VoskModel", repo_type="dataset", local_dir=get_resources_path(), max_workers=10)
 
 
 __all__ = ["download_resources"]
